a670c5ab3f8a1dcf5eb187e1cea428c5fe051de2
givenchy <113390160+givxnchy@users.noreply.github.com>
2023-02-20 16:07:09 +0300

    Delete first_task directory

--- first_task/src/OperatorsTask3/Dog.java
+++
@@ -1,6 +0,0 @@
-package OperatorsTask3;
-
-public class Dog {
-    String name;
-    String says;
-}
--- first_task/src/concept_task/Dog.java
+++
@@ -1,9 +0,0 @@
-package concept_task;
-
-public interface Dog {
-    void changeNumberOfPaws(int newValue);
-    void changeNameOfBreed(String changeBreed);
-    void changeGender(String changeGndr);
-    void changeAge(int newValue);
-
-}
