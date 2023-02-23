47626aa114d6fe6739fae17bcb5527b783f9c365
givenchy <113390160+givxnchy@users.noreply.github.com>
2023-02-23 12:28:11 +0300

    Add files via upload

---
+++ JavaIntern/primitive_conversion_task/src/main/java/iba/eu/java/education/conversionClass/primitive_conv.java
@@ -0,0 +1,24 @@
+package iba.eu.java.education.conversionClass;
+
+public class primitive_conv {
+    byte primConvertToByte(byte b){
+        byte result = 0;
+        //result = (b+1000)*2;
+        return result;
+    }
+
+    int primConvertToInt(int i){
+        int result = 0;
+        //result = (i+2147483648)*2;
+        return result;
+    }
+
+    long primConvertToLong(long l){
+        long result = 0;
+        //result = (l+9223372036854775807)*10000000000;
+        return result;
+    }
+
+    //?????????????
+    //Надо ли сделать main и фикс проблем в телах методов
+}
