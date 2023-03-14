851664bc9364543323933e924533f6de3cc50178
givenchy <113390160+givxnchy@users.noreply.github.com>
2023-03-14 22:09:03 +0300

    Delete internJava directory

--- internJava/concept_task/src/main/java/iba/eu/java/education/conceptClass/Dog.java
+++
@@ -1,8 +0,0 @@
-package iba.eu.java.education.conceptClass;
-
-public interface Dog {
-    void changeNumberOfPaws(int newValue);
-    void changeNameOfBreed(String changeBreed);
-    void changeGender(String changeGndr);
-    void changeAge(int newValue);
-}
